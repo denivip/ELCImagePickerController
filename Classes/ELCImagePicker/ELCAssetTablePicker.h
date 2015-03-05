70da58bf5a8dbf1fa31ac5ffcb8deeaa1ef10a9d
IPv6 <ipv6@Nikolays-iMac.local>
2015-03-05 19:29:07 +0300

    grouping is optional

--- Classes/ELCImagePicker/ELCAssetTablePicker.h
+++ Classes/ELCImagePicker/ELCAssetTablePicker.h
@@ -16,16 +16,15 @@
 @property (nonatomic, strong) ALAssetsLibrary *library;
 @property (nonatomic, weak) id <ELCAssetSelectionDelegate> delegate;
 @property (nonatomic, strong) ALAssetsGroup *assetGroup;
-@property (nonatomic, strong) NSMutableArray *elcAssets;
+@property (nonatomic, strong) NSMutableArray *elcAssetsBySection;
 @property (nonatomic, strong) IBOutlet UILabel *selectedAssetsLabel;
 @property (nonatomic) BOOL singleSelection;
 @property (nonatomic) BOOL immediateReturn;
+@property (nonatomic) BOOL groupByDate;
 
 - (int)totalSelectedAssets;
 - (void)preparePhotos;
-
 - (void)doneAction:(id)sender;
-
 - (void)assetSelected:(ELCAsset *)asset;
 
 @end
--- Classes/ELCImagePicker/ELCAssetTablePicker.m
+++ Classes/ELCImagePicker/ELCAssetTablePicker.m
@@ -7,8 +7,11 @@
 
 #import "ELCAssetTablePicker.h"
 #import "ELCAssetCell.h"
+#import "DVTogetherAppearance.h"
 #import "DVGTogetherAppearance.h"
 
+static float kELCSectionTitleHeight = 20.0f;
+static float kELCSectionTitleTopSpace = 20.0f;
 @interface ELCAssetTablePicker ()
 
 @property (nonatomic) NSInteger columns;
@@ -32,8 +35,7 @@
 	[self.tableView setAllowsSelection:NO];
     self.tableView.backgroundColor = [UIColor togetherBackgroundColor];
 
-    NSMutableArray *tempArray = [[NSMutableArray alloc] init];
-    self.elcAssets = tempArray;
+    self.elcAssetsBySection = @[].mutableCopy;
 	
     if (self.immediateReturn) {
         
@@ -74,20 +76,65 @@
 - (void)preparePhotos
 {
     @autoreleasepool {
-
+        self.elcAssetsBySection = @[].mutableCopy;
+        __block NSMutableArray* elcAssets = @[].mutableCopy;
         [self.assetGroup enumerateAssetsUsingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
-
             if(result == nil) {
                 return;
             }
-
             ELCAsset *elcAsset = [[ELCAsset alloc] initWithAsset:result];
             [elcAsset setParent:self];
             NSString *url = [[result valueForProperty:ALAssetPropertyAssetURL] absoluteString];
             elcAsset.enabled = ![self.disabledURLs containsObject:url];
-            [self.elcAssets addObject:elcAsset];
+            [elcAssets addObject:elcAsset];
         }];
-
+        
+        if(self.groupByDate){
+            // Grouping assets by date
+            [elcAssets sortUsingComparator:^NSComparisonResult(ELCAsset *obj1, ELCAsset *obj2) {
+                NSDate *date1 = [obj1.asset valueForProperty:ALAssetPropertyDate];
+                if (! date1 ||
+                    [date1 compare:[NSDate distantPast]] == NSOrderedAscending ||
+                    [date1 compare:[NSDate distantFuture]] == NSOrderedDescending) {
+                    // Если даты нет или она некорректная, то задаем текущую.
+                    date1 = [NSDate date];
+                }
+                NSDate *date2 = [obj2.asset valueForProperty:ALAssetPropertyDate];
+                if (! date2 ||
+                    [date2 compare:[NSDate distantPast]] == NSOrderedAscending ||
+                    [date2 compare:[NSDate distantFuture]] == NSOrderedDescending) {
+                    // Если даты нет или она некорректная, то задаем текущую.
+                    date2 = [NSDate date];
+                }
+                return -1*[date1 compare:date2];
+            }];
+            static NSDateFormatter *dateFormatter = nil;
+            if (dateFormatter == nil) {
+                dateFormatter = [[NSDateFormatter alloc] init];
+                //[dateFormatter setLocale:[NSLocale applicationCurrentLocale]];
+                [dateFormatter setMonthSymbols:dateFormatter.standaloneMonthSymbols];
+                [dateFormatter setDateFormat:@"d,MMMM,yyyy"];
+            }
+            NSString* sectionKey = @"";
+            for(ELCAsset* elcAsset in elcAssets){
+                NSDate *assetDate = [elcAsset.asset valueForProperty:ALAssetPropertyDate];
+                if (! assetDate ||
+                    [assetDate compare:[NSDate distantPast]] == NSOrderedAscending ||
+                    [assetDate compare:[NSDate distantFuture]] == NSOrderedDescending) {
+                    // Если даты нет или она некорректная, то задаем текущую.
+                    assetDate = [NSDate date];
+                }
+                NSString* assetKey = [dateFormatter stringFromDate:assetDate];
+                if([assetKey compare:sectionKey] != NSOrderedSame){
+                    NSMutableArray* section = @[].mutableCopy;
+                    [self.elcAssetsBySection addObject:section];
+                    sectionKey = assetKey;
+                }
+                [[self.elcAssetsBySection lastObject] addObject:elcAsset];
+            }
+        }else{
+            [self.elcAssetsBySection addObject:elcAssets];
+        }
         dispatch_sync(dispatch_get_main_queue(), ^{
             [self.tableView reloadData];
             // scroll to bottom
@@ -101,7 +148,8 @@
                                               animated:NO];
             }
             
-            if (self.elcAssets.count && [((ELCAsset *)[self.elcAssets objectAtIndex:0]).asset valueForProperty:ALAssetPropertyType] == ALAssetTypePhoto) {
+            if (self.elcAssetsBySection.count && [[self.elcAssetsBySection objectAtIndex:0] count]
+                && [((ELCAsset *)[[self.elcAssetsBySection objectAtIndex:0] objectAtIndex:0]).asset valueForProperty:ALAssetPropertyType] == ALAssetTypePhoto) {
                 [self.navigationItem setTitle:self.singleSelection ? NSLocalizedString(@"IMPORT_LIBRARY_PHOTO_SINGLE", @"[Title bar title] single selection") : NSLocalizedString(@"IMPORT_LIBRARY_PHOTO_MANY", @"[Title bar title] multiple selection")];
             } else {
                 [self.navigationItem setTitle:self.singleSelection ? NSLocalizedString(@"IMPORT_LIBRARY_VIDEO_SINGLE", @"[Title bar title] single selection") : NSLocalizedString(@"IMPORT_LIBRARY_VIDEO_MANY", @"[Title bar title] multiple selection")];
@@ -113,13 +161,12 @@
 - (void)doneAction:(id)sender
 {	
 	NSMutableArray *selectedAssetsImages = [[NSMutableArray alloc] init];
-	    
-	for(ELCAsset *elcAsset in self.elcAssets) {
-
-		if([elcAsset selected]) {
-			
-			[selectedAssetsImages addObject:[elcAsset asset]];
-		}
+    for(NSArray *assets in self.elcAssetsBySection) {
+        for(ELCAsset *elcAsset in assets) {
+            if([elcAsset selected]) {
+                [selectedAssetsImages addObject:[elcAsset asset]];
+            }
+        }
 	}
 
     [self.delegate elc_assetSelection:self didSelectAssets:selectedAssetsImages library:self.library];
@@ -128,10 +175,11 @@
 - (void)assetSelected:(ELCAsset*)asset
 {
     if (self.singleSelection) {
-
-        for(ELCAsset *elcAsset in self.elcAssets) {
-            if(asset != elcAsset) {
-                elcAsset.selected = NO;
+        for(NSArray *assets in self.elcAssetsBySection) {
+            for(ELCAsset *elcAsset in assets) {
+                if(asset != elcAsset) {
+                    elcAsset.selected = NO;
+                }
             }
         }
     }
@@ -150,20 +198,73 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return 1;
+    return [self.elcAssetsBySection count];
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    if(!self.groupByDate){
+        return 0;
+    }
+    if(section < 0 || section >= [self.elcAssetsBySection count]){
+        return 0;
+    }
+    NSArray* sectionAssets = [self.elcAssetsBySection objectAtIndex:section];
+    if([sectionAssets count] == 0){
+        return 0;
+    }
+    return kELCSectionTitleHeight+kELCSectionTitleTopSpace;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    if(!self.groupByDate){
+        return nil;
+    }
+    if(section < 0 || section >= [self.elcAssetsBySection count]){
+        return nil;
+    }
+    NSArray* sectionAssets = [self.elcAssetsBySection objectAtIndex:section];
+    if([sectionAssets count] == 0){
+        return nil;
+    }
+    ELCAsset* firstAsset = [sectionAssets objectAtIndex:0];
+    NSDate *assetDate = [firstAsset.asset valueForProperty:ALAssetPropertyDate];
+    if (! assetDate ||
+        [assetDate compare:[NSDate distantPast]] == NSOrderedAscending ||
+        [assetDate compare:[NSDate distantFuture]] == NSOrderedDescending) {
+        // Если даты нет или она некорректная, то задаем текущую.
+        assetDate = [NSDate date];
+    }
+
+    static NSDateFormatter *dateFormatter = nil;
+    if (dateFormatter == nil) {
+        dateFormatter = [[NSDateFormatter alloc] init];
+        //[dateFormatter setLocale:[NSLocale applicationCurrentLocale]];
+        dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+        [dateFormatter setMonthSymbols:dateFormatter.standaloneMonthSymbols];
+    }
+    UIView* titleWrapper = [[UIView alloc] initWithFrame:CGRectMake(0.0f,0.0f,self.view.frame.size.width,kELCSectionTitleHeight+kELCSectionTitleTopSpace)];
+    UILabel* title = [[UILabel alloc] initWithFrame:CGRectMake(10.0f,kELCSectionTitleTopSpace,self.view.frame.size.width,kELCSectionTitleHeight)];
+    [title applyTogetherAppearance];
+    title.text = [dateFormatter stringFromDate:assetDate];
+    [titleWrapper addSubview:title];
+    return titleWrapper;
+}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return ceil([self.elcAssets count] / (float)self.columns);
+    if(section < 0 || section >= [self.elcAssetsBySection  count]){
+        return 0;
+    }
+    NSArray* sectionAssets = [self.elcAssetsBySection objectAtIndex:section];
+    return ceil([sectionAssets count] / (float)self.columns);
 }
 
 - (NSArray *)assetsForIndexPath:(NSIndexPath *)path
 {
+    NSArray* sectionAssets = [self.elcAssetsBySection objectAtIndex:path.section];
     NSInteger index = path.row * self.columns;
-    NSInteger length = MIN(self.columns, [self.elcAssets count] - index);
-    return [self.elcAssets subarrayWithRange:NSMakeRange(index, length)];
+    NSInteger length = MIN(self.columns, [sectionAssets count] - index);
+    return [sectionAssets subarrayWithRange:NSMakeRange(index, length)];
 }
 
 // Customize the appearance of table view cells.
@@ -192,12 +293,14 @@
 - (int)totalSelectedAssets {
     
     int count = 0;
-    
-    for(ELCAsset *asset in self.elcAssets) {
-		if([asset selected]) {   
-            count++;	
-		}
-	}
+
+    for(NSArray *assets in self.elcAssetsBySection) {
+        for(ELCAsset *asset in assets) {
+            if([asset selected]) {
+                count++;
+            }
+        }
+    }
     
     return count;
 }
